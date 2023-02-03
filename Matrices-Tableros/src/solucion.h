093a6da5dd6d64d31eefc1f76c7ce86329df9ff0
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-02-03 17:03:26 -0300

    Header actualizado con mas funciones

--- Matrices-Tableros/src/solucion.h
+++ Matrices-Tableros/src/solucion.h
@@ -24,6 +24,24 @@ bool esPico(vector <vector <int > > m, int f, int c);
 
 tuple<tuple<int, int>, vector<tuple<int, int, int>>> aTriplas(vector<vector<int> > m);
 
+vector<int> triplasAVector(vector<tuple<int, int, int>> m);
+vector <vector<int > > aMatriz(vector<tuple<int, int, int>> m, tuple<int, int> dim);
+
+void transponerDispersa(vector<tuple<int, int, int>>& m);
+
+void elevar(vector<vector<int>> &terreno, int x);
+
+bool sobresalen(vector<vector<int>> terreno, int n, int &mts);
+
+int contarCeldasSubacuaticas(vector<vector<int>> m, int f, int c);
+int cantCeldasVecinas(vector<vector<int>> t, int f, int c);
+int islas(vector<vector<int>> terreno);
+
+tuple<int, int> buscarValleDesde(vector< vector<int>> m, int fila, int columna);
+bool todosVecinosAdyacentesMenores(vector< vector<int>> m, int fila, int columna);
+
+
+
 #endif 
 
 //L07_MATRICESYTABLEROS_H
