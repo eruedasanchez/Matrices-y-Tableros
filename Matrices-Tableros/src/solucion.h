87c430cc6828582ee992ce1bff184b750e9b7299
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-02-02 22:21:35 -0300

    Modificaciones al header

--- Matrices-Tableros/src/solucion.h
+++ Matrices-Tableros/src/solucion.h
@@ -6,6 +6,7 @@
 #include <vector>
 #include <string>
 #include <assert.h>
+#include <tuple>
 
 using namespace std;
 
@@ -13,7 +14,15 @@ void mostrarMatriz(vector <vector <int > > m);
 
 int productoPunto(vector<int> v1, vector<int> v2);
 
-vector <vector <int > > productoConTraspuesta(vector <vector <int > > A);
+vector<int> transformarEnVector(vector <vector <int > > m);
+vector <vector <int > > redimensionar(vector <vector <int > > m, int filas, int columnas);
+
+void trasponer(vector <vector <int > >& m);
+
+int contarPicos(vector <vector <int > > m);
+bool esPico(vector <vector <int > > m, int f, int c);
+
+tuple<tuple<int, int>, vector<tuple<int, int, int>>> aTriplas(vector<vector<int> > m);
 
 #endif 
 
