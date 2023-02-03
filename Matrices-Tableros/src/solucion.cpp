b51db2c22d925920796a8778aea2850ded88ff3d
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-02-02 22:18:19 -0300

    Resolucion hasta el ejercicio 6.a

--- Matrices-Tableros/src/solucion.cpp
+++ Matrices-Tableros/src/solucion.cpp
@@ -56,19 +56,206 @@ A            At             res
 /* Ejercicio 3 */
 
  /* La precondicion en el ejercicio 3 es que la cantidad total de elementos de la matriz tiene que ser divisible
-por la cantidad de columnas que se quiere redimensionar */
+por la cantidad de filas que se quiere redimensionar */
+
+vector<int> transformarEnVector(vector <vector <int > > m){
+    vector<int> vec;
+
+    for(int fila = 0; fila < m.size(); fila++){
+        for(int columna = 0; columna < m[fila].size(); columna++){
+            vec.push_back(m[fila][columna]);
+        }
+    }
+    return vec;
+} 
 
 vector <vector <int > > redimensionar(vector <vector <int > > m, int filas, int columnas){
-    assert((m.size() > 0) && (m.size() * m[0].size() % columnas == 0));                      // PRE: Mencionada al inicio del ejercicio
+    assert((m.size() > 0) && (m.size() * m[0].size() % filas == 0));                      // PRE: Mencionada al inicio del ejercicio
     
     vector <vector <int > > matrizRedimensionada;
-    vector<int> filaActualRed;
+    vector<int> vectorMatriz = transformarEnVector(m);
+    int j = 0;
+    vector<int> filaActual;
     
-
+    for(int i = 0; i < vectorMatriz.size(); i++){
+        if(j < columnas){
+            filaActual.push_back(vectorMatriz[i]);
+            j++;
+        } else {
+            j = 0;
+            matrizRedimensionada.push_back(filaActual);
+            vector<int> filaActual;
+        }
+    }
     return matrizRedimensionada;
-
-
 }
+
+/* Ejercicio 4 */
+
+void trasponer(vector <vector <int > >& m){
+    for(int fila = 0; fila < m.size(); fila++){
+        for(int columna = 0; columna < m[fila].size(); columna++){
+            m[fila][columna] = m[columna][fila];
+        }
+    }
+}
+
+/* Ejercicio 5 */
+
+int contarPicos(vector <vector <int > > m){
+    int cantPicos = 0;
+    for(int fila = 0; fila < m.size(); fila++){
+        for(int columna = 0; columna < m[fila].size(); columna++){
+            if(esPico(m,fila,columna)){
+                cantPicos++;
+            }
+        }
+    }
+    return cantPicos;
+}
+
+bool esPico(vector <vector <int > > m, int f, int c){
+    bool pico = true;
+    if(f == 0){
+        /* Caso 1. Primer fila de la matriz */
+        if(c == 0){
+            /* Caso 1.a. Primer fila y primer columna */
+            for(int fila = f; fila <= f+1 ; fila++){
+                for(int columna = c; columna <= c+1; columna++){
+                    if(m[fila][columna] > m[f][c]){
+                        pico = false;
+                        break;
+                    }
+                }
+            }
+        } else if(c == m.size()-1){
+            /* Caso 1.b Primer fila y ultima columna */
+            for(int fila = f; fila <= f+1 ; fila++){
+                for(int columna = c-1; columna <= c; columna++){
+                    if(m[fila][columna] > m[f][c]){
+                        pico = false;
+                        break;
+                    }
+                }
+            }
+        } else {
+            /* Caso 1.c Primer fila y columnas del "medio" */
+            for(int fila = f; fila <= f+1 ; fila++){
+                for(int columna = c-1; columna <= c+1; columna++){
+                    if(m[fila][columna] > m[f][c]){
+                        pico = false;
+                        break;
+                    }
+                }
+            }
+        }
+    } else if(f == m.size()-1){
+        /* Caso 2 Ultima fila de la matriz */
+        if(c == 0){ 
+            /* Caso 2.a Ultima fila y primer columna */
+            for(int fila = f-1; fila <= f ; fila++){
+                for(int columna = c; columna <= c+1; columna++){
+                    if(m[fila][columna] > m[f][c]){
+                        pico = false;
+                        break;
+                    }
+                }
+            }
+        } else if(c == m.size() - 1){
+            /* Caso 2.b Ultima fila y ultima columna */
+            for(int fila = f-1; fila <= f ; fila++){
+                for(int columna = c-1; columna <= c; columna++){
+                    if(m[fila][columna] > m[f][c]){
+                        pico = false;
+                        break;
+                    }
+                }
+            }
+        } else {
+            /* Caso 2.c Ultima fila y columnas del "medio" */
+            for(int fila = f-1; fila <= f ; fila++){
+                for(int columna = c-1; columna <= c+1; columna++){
+                    if(m[fila][columna] > m[f][c]){
+                        pico = false;
+                        break;
+                    }
+                }
+            }
+        }
+    /* Casos de filas del medio */
+    } else if(c == 0){
+        /* Caso 3.a Cualquier fila del medio y primera columna */
+        for(int fila = f-1; fila+1 <= f ; fila++){
+                for(int columna = c; columna <= c+1; columna++){
+                    if(m[fila][columna] > m[f][c]){
+                        pico = false;
+                        break;
+                    }
+                }
+            }
+    } else if (c == m.size()-1){
+        /* Caso 3.b Cualquier fila del medio y ultima columna */
+        for(int fila = f-1; fila+1 <= f ; fila++){
+                for(int columna = c-1; columna <= c; columna++){
+                    if(m[fila][columna] > m[f][c]){
+                        pico = false;
+                        break;
+                    }
+                }
+            }
+    } else {
+        /* Caso 4 Cualquier otro valor que no se encuentra en los bordes de la matriz */
+        for(int fila = f-1; fila <= f+1 ; fila++){
+            for(int columna = c-1; columna <= c+1; columna++){
+                if(m[fila][columna] > m[f][c]){
+                    pico = false;
+                    break;
+                }
+            }
+
+        }
+    }
+    return pico;
+}
+/* Ejemplo de matriz para resolver el ejercicio */
+
+// 1 2 3  
+// 4 5 6 
+// 7 8 9  
+
+// i = 1
+// j = 2
+
+// a = 0 -> b = 1,2
+// a = 1 -> b = 1,2
+// a = 2 -> b = 1,2
+
+/* Ejercicio 6.a */
+
+tuple<tuple<int, int>, vector<tuple<int, int, int>>> aTriplas(vector<vector<int> > m){
+
+    /* Creacion de la tupla con la cantidad de filas y columnas de la matriz */
+    int filasMatriz = m.size();
+    int columnasMatriz = m[0].size();
+    tuple<int, int> filasYColumnas = make_tuple(filasMatriz, columnasMatriz);
+
+    /* A continuacion, se arma el vector con los elementos distintos de cero detallando sus coordenadas */
+    vector<tuple<int, int, int>> distintosCero;
+    for(int fila = 0; fila < m.size(); fila++){
+        for(int columna = 0; columna < m[fila].size(); columna++){
+            if(m[fila][columna] != 0){
+                tuple<int, int, int> elemYCoordenadas = make_tuple(m[fila][columna], fila, columna);
+                distintosCero.push_back(elemYCoordenadas);
+            }
+        }
+    }
+
+    /* Finalmente, se arma la tupla con ambos resultados */
+    tuple<tuple<int, int>, vector<tuple<int, int, int>>> resultado = make_tuple(filasYColumnas, distintosCero);
+    return resultado;
+}
+
+
 
 
 
