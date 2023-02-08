8648ca5a0a99359d807a9ad715eb0362ba6de7e1
eruedasanchez <ezequiel.ruedasanchez@gmail.com>
2023-02-08 18:57:57 -0300

    Header de los primeros 3 ejercicios

--- Matrices-Tableros/src/solucion.h
+++ Matrices-Tableros/src/solucion.h
@@ -4,48 +4,31 @@
 
 #include <iostream>
 #include <vector>
-#include <string>
+#include <string.h>
 #include <assert.h>
 #include <tuple>
 
 using namespace std;
 
-void mostrarMatriz(vector <vector <int > > m);
+void mostrarVector(vector<int> &v);
 
+/* Ejercicio 1 */
+void mostrarMatriz(vector<vector<int>> &m);
+
+/* Ejercicio 2.a */
 int productoPunto(vector<int> v1, vector<int> v2);
 
-vector<int> transformarEnVector(vector <vector <int > > m);
-vector <vector <int > > redimensionar(vector <vector <int > > m, int filas, int columnas);
+/* Ejercicio 2.b */
+vector<vector<int>> productoConTraspuesta(vector<vector<int>> A);
 
-void trasponer(vector <vector <int > >& m);
-
-int contarPicos(vector <vector <int > > m);
-bool esPico(vector <vector <int > > m, int f, int c);
-
-tuple<tuple<int, int>, vector<tuple<int, int, int>>> aTriplas(vector<vector<int> > m);
-
-vector<int> triplasAVector(vector<tuple<int, int, int>> m);
-vector <vector<int > > aMatriz(vector<tuple<int, int, int>> m, tuple<int, int> dim);
-
-void transponerDispersa(vector<tuple<int, int, int>>& m);
-
-void elevar(vector<vector<int>> &terreno, int x);
-
-bool sobresalen(vector<vector<int>> terreno, int n, int &mts);
-
-int contarCeldasSubacuaticas(vector<vector<int>> m, int f, int c);
-int cantCeldasVecinas(vector<vector<int>> t, int f, int c);
-int islas(vector<vector<int>> terreno);
-
-tuple<int, int> buscarValleDesde(vector< vector<int>> m, int fila, int columna);
-bool todosVecinosAdyacentesMenores(vector< vector<int>> m, int fila, int columna);
-
-bool reinasEnAmenaza(vector< vector<char>> tablero, tuple<int, int> dimensiones);
-bool estaAmenazada(vector< vector<char>> t, int fila, int columna);
+/* Ejercicio 3 */
+vector<int> transformarEnVector(vector<vector<int>> m);
+vector<vector<int>> redimensionar(vector<vector<int>> m, int filas, int columnas);
 
 
 
-#endif 
+
+#endif
 
 //L07_MATRICESYTABLEROS_H
 
